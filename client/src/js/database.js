7de07bce1d002100887e6c86678f103fad61eb66
Miguel Torre <m.a.torre.f@gmail.com>
2022-05-07 09:37:33 -0700

    started with idb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,6 +16,18 @@ const initdb = async () =>
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () =>{
+  console.log('Getting data from the jateDB');
+  const db = await openDB('jate', 1);
+  // make new transaction...need to specify the DB we are posting to and the data privileges. 
+  const tx = db.transaction('jate', 'readwrite');
+  // open the object store
+  const objStore = tx.objectStore('jate');
+  // use the .getAll() method to grab all the content in the DB
+  const req = objStore.getAll()
+  // confirm the data was fetched
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+ console.error('getDb not implemented');
+};
 initdb();
