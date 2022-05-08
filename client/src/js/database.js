6811d5e0b794b2d47af8fd9cc1c98156043f16d4
Miguel Torre <m.a.torre.f@gmail.com>
2022-05-08 15:43:26 -0700

    added put function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,21 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT request to update the jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({ id: id, value: value })
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () =>{
-  console.log('Getting data from the jateDB');
   const db = await openDB('jate', 1);
-  // make new transaction...need to specify the DB we are posting to and the data privileges. 
   const tx = db.transaction('jate', 'readwrite');
-  // open the object store
   const objStore = tx.objectStore('jate');
-  // use the .getAll() method to grab all the content in the DB
   const req = objStore.getAll()
-  // confirm the data was fetched
   const res = await req;
   console.log('data saved to the jateDB', res);
- console.error('getDb not implemented');
 };
 initdb();
